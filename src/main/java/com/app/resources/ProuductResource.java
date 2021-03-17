17743b012943d29e057e1dd5428b5367ade40df0
Gabriel B <gabrielbicalho.eloi@gmail.com>
2021-03-17 13:33:22 -0300

    feat: implementando Swagger

--- src/main/java/com/app/resources/ProuductResource.java
+++ src/main/java/com/app/resources/ProuductResource.java
@@ -6,16 +6,19 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.app.dto.ProductDTO;
 import com.app.models.Product;
 import com.app.services.ProductService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/starstore")
@@ -24,14 +27,26 @@ public class ProuductResource {
 	@Autowired
 	private ProductService service;
 	
-	@GetMapping("/products")
+	//@GetMapping("/products")
+	@ApiOperation(value = "Retorna uma lista de Produtos")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Retorna a lista de produtos"),
+			@ApiResponse(code = 500, message = "Foi gerada uma exceção")
+	})
+	@RequestMapping(value = "/products", method = RequestMethod.GET, produces="application/json")
 	public ResponseEntity<List<ProductDTO>> findAll(){
 		List<Product> list = service.findAll();
 		List<ProductDTO> listDTO = list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
-	@PostMapping("/product")
+	//@PostMapping("/product")
+	@ApiOperation(value = "Insere um produto ao banco de dados")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Produto foi adicionado ao banco de dados"),
+			@ApiResponse(code = 500, message = "Foi gerada uma exceção")
+	})
+	@RequestMapping(value = "/product", method = RequestMethod.POST, produces="application/json")
 	public ResponseEntity<Product> insert(@RequestBody Product obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
--- src/main/java/com/app/resources/TransationResource.java
+++ src/main/java/com/app/resources/TransationResource.java
@@ -6,17 +6,20 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.app.dto.TransationDTO;
 import com.app.models.Transation;
 import com.app.services.TransationService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/starstore")
@@ -25,7 +28,13 @@ public class TransationResource {
 	@Autowired
 	private TransationService service;
 	
-	@PostMapping("/buy")
+	//@PostMapping("/buy")
+	@ApiOperation(value = "Insere dados de uma compra, junto com os dados do cliente")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Dados inseridos com sucesso"),
+			@ApiResponse(code = 500, message = "Foi gerada uma exceção")
+	})
+	@RequestMapping(value = "/buy", method = RequestMethod.POST, produces="application/json")
 	public ResponseEntity<Transation> insert(@RequestBody Transation obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -33,7 +42,13 @@ public class TransationResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@GetMapping("/history")
+	//@GetMapping("/history")
+	@ApiOperation(value = "Retorna o historico de todas as compras efetuadas")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Retorna a lista de compras"),
+			@ApiResponse(code = 500, message = "Foi gerada uma exceção")
+	})
+	@RequestMapping(value = "/history", method = RequestMethod.GET, produces="application/json")
 	public ResponseEntity<List<TransationDTO>>  findAll(){
 		List<Transation> list = service.findAll();
 		List<TransationDTO> listDTO = list.stream().map(x -> new TransationDTO(x)).collect(Collectors.toList());
@@ -41,7 +56,14 @@ public class TransationResource {
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
-	@GetMapping("/history/{clientId}")
+	//@GetMapping("/history/{clientId}")
+	@ApiOperation(value = "Retorna uma lista de compras de um cliente especifico")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Retorna a lista de compras"),
+			@ApiResponse(code = 404, message = "O cliente especificado não existe"),
+			@ApiResponse(code = 500, message = "Foi gerada uma exceção")
+	})
+	@RequestMapping(value = "/history/{clientId}", method = RequestMethod.GET, produces="application/json")
 	public ResponseEntity<List<TransationDTO>> findByClientId(@PathVariable String clientId){
 		List<Transation> list = service.findByClientId(clientId);
 		if(list.isEmpty()) {
